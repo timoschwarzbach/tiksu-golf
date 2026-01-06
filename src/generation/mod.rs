98fcc7a52d55c295f05ee3a67bfc4092358f5956
Louis Goerlt <louis.goerlt@gmail.com>
2026-01-06 02:39:43 +0200

    code side course generation

--- src/generation/mod.rs
+++ src/generation/mod.rs
@@ -1,3 +1,5 @@
+use crate::material::ground::Polynomial;
+
 pub mod grasslands;
 
 pub enum PropType {
@@ -13,4 +15,7 @@ pub struct Prop {
 pub trait TerrainGenerator {
     fn height_at(&self, x: f32, y: f32) -> f32;
     fn props_in_chunk(&self, offset: (i32, i32)) -> Vec<Prop>;
+    fn course_layout(&self) -> Polynomial;
+    fn start(&self) -> [f32; 2];
+    fn hole(&self) -> [f32; 2];
 }
