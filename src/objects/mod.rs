c4824cbffbac1147d3b9d43129f961139a5fa970
timoschwarzbach <timo.schwarzbach@outlook.com>
2026-01-11 03:15:19 +0200

    added aim tiksu

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -1,2 +1,3 @@
+pub mod aim_tiksu;
 pub mod flag_pole;
 pub mod golfball;
--- src/state/aim.rs
+++ src/state/aim.rs
@@ -8,6 +8,7 @@ use bevy::{
 };
 
 use crate::camera::ActiveCamera;
+use crate::objects::aim_tiksu::AimTiksuPlugin;
 use crate::objects::flag_pole::FlagPole;
 use crate::objects::golfball::Golfball;
 use crate::state::state::AppState;
@@ -19,7 +20,7 @@ impl Plugin for AimStatePlugin {
         app.add_systems(PostStartup, set_aim_state.run_if(in_state(AppState::Aim)))
             .add_systems(
                 Update,
-                (input_handler)
+                (input_handler, aim_camera_position_system)
                     .run_if(in_state(AppState::Aim))
                     .run_if(in_state(AimChallengeState::Idle)),
             )
@@ -28,56 +29,60 @@ impl Plugin for AimStatePlugin {
             .add_systems(
                 OnEnter(AimChallengeState::Finalized),
                 execute_golfball_punch,
-            );
+            )
+            .add_plugins(AimTiksuPlugin);
     }
 }
 
 #[derive(Resource)]
-struct AimState {
-    selected_club: u8,
-    zoomed: bool,
+pub struct AimState {
+    pub selected_club: u8,
+    pub rotation: f32,
+    pub zoomed: bool,
 }
 
 #[derive(Component)]
 pub struct AimCamera;
 
-fn set_aim_state(
-    mut commands: Commands,
-    camera: Single<(Entity, &ActiveCamera, &mut Transform)>,
+fn set_aim_state(mut commands: Commands, camera: Single<Entity, With<ActiveCamera>>) {
+    // update camera bundle
+    commands.entity(camera.entity()).insert(AimCamera);
+
+    // set up state resource
+    commands.insert_resource(AimState {
+        selected_club: 0,
+        rotation: 0.0,
+        zoomed: false,
+    });
+}
+
+fn aim_camera_position_system(
+    mut camera: Single<&mut Transform, With<ActiveCamera>>,
     golfball: Single<(&Golfball, &Transform), Without<ActiveCamera>>,
     flag_pole: Single<(&FlagPole, &Transform), Without<ActiveCamera>>,
+    aim_state: If<Res<AimState>>,
 ) {
     // get golfball position
     let golfball_position = golfball.1;
     // get flag pole position
     let flag_position = flag_pole.1;
-    let flag_direction_vector =
-        (flag_position.translation - golfball_position.translation).normalize();
+    let look_direction_vector = (flag_position.translation - golfball_position.translation)
+        .normalize()
+        .rotate_y(aim_state.rotation);
 
-    // set camera 5m in front of golfball looking at flag pole
+    // set camera 5m in front of golfball looking at golfball
     let camera_position =
-        golfball_position.translation + flag_direction_vector * -5.0 + vec3(0.0, 1.5, 0.0); // what if this is in the ground?
+        golfball_position.translation + look_direction_vector * -5.0 + vec3(0.0, 1.5, 0.0); // what if this is in the ground?
     let aim_camera_transform = Transform::from_translation(camera_position).looking_at(
         Vec3::new(
-            flag_position.translation.x,
+            golfball_position.translation.x,
             camera_position.y,
-            flag_position.translation.z,
+            golfball_position.translation.z,
         ),
         Vec3::Y,
     );
 
-    let (camera_entity, _, mut camera_transform) = camera.into_inner();
-    *camera_transform = aim_camera_transform;
-
-    // update camera bundle
-    commands.entity(camera_entity).insert(AimCamera);
-    info!("set camera to aim position");
-
-    // set up state resource
-    commands.insert_resource(AimState {
-        selected_club: 0,
-        zoomed: false,
-    });
+    **camera = aim_camera_transform;
 }
 
 fn unset_aim_state(mut commands: Commands, camera: Single<Entity, With<AimCamera>>) {
@@ -91,15 +96,14 @@ fn unset_aim_state(mut commands: Commands, camera: Single<Entity, With<AimCamera
 fn input_handler(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut aim_state: If<ResMut<AimState>>,
-    mut camera: Single<&mut Transform, With<AimCamera>>,
     time: Res<Time>,
 ) {
     let multiplier = 0.25;
     if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        camera.rotate_y(multiplier * time.delta_secs());
+        aim_state.rotation += multiplier * time.delta_secs();
     }
     if keyboard_input.pressed(KeyCode::ArrowRight) {
-        camera.rotate_y(-multiplier * time.delta_secs());
+        aim_state.rotation -= multiplier * time.delta_secs();
     }
     if keyboard_input.just_pressed(KeyCode::ArrowUp) {
         aim_state.zoomed = true;
